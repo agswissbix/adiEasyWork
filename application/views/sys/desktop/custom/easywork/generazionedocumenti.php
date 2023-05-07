1f10cceaae5a890f2bcf2649a3d171dcb25ec962
agswissbix <108857221+agswissbix@users.noreply.github.com>
2023-05-08 00:42:06 +0200

    .

---
+++ application/views/sys/desktop/custom/easywork/generazionedocumenti.php
@@ -0,0 +1,184 @@
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script type="text/javascript">
+     // Or with jQuery
+
+  $(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+      data: {
+        <?php
+        foreach ($candidati as $key => $candidato) {
+        ?>
+            "<?=$candidato['Cognome'].' '.$candidato['Nome'].'-'.$candidato['ID']?>": null,
+        <?php
+        }
+        ?>
+
+      }
+    });
+    
+    $('.modal').modal();
+    $('.datepicker').datepicker({
+    format: 'dd/mm/yyyy'
+    });
+  });
+  
+  function crea()
+  {
+        $('#modal1').modal('open');
+        var serialized=$('#generazionedocumenti').find("select,textarea,input").serializeArray();
+        $.ajax( {
+            type: "POST",
+            url: '<?=site_url('sys_viewcontroller/custom_easywork_generadocumenti')?>',
+            data: serialized,
+            success: function( response ) {
+            },
+            error:function(){
+            }
+        } );
+  }
+  
+  function converti()
+  {
+      $('#modal1').modal('open');
+        var serialized=$('#generazionedocumenti').find("select,textarea,input").serializeArray();
+        $.ajax( {
+            type: "POST",
+            url: '<?=site_url('sys_viewcontroller/custom_easywork_convertidocumenti')?>',
+            data: serialized,
+            success: function( response ) {
+            },
+            error:function(){
+            }
+        } );
+  }
+  
+  function creaconverti()
+  {
+        $('#modal1').modal('open');
+        var serialized=$('#generazionedocumenti').find("select,textarea,input").serializeArray();
+        $.ajax( {
+            type: "POST",
+            url: '<?=site_url('sys_viewcontroller/custom_easywork_generadocumenti/true')?>',
+            data: serialized,
+            success: function( response ) {
+            },
+            error:function(){
+            }
+        } );
+  }
+  
+    function creacopertina()
+  {
+        $('#modal1').modal('open');
+        var serialized=$('#generazionedocumenti').find("select,textarea,input").serializeArray();
+        $.ajax( {
+            type: "POST",
+            url: '<?=site_url('sys_viewcontroller/custom_easywork_generacopertina')?>',
+            data: serialized,
+            success: function( response ) {
+            },
+            error:function(){
+            }
+        } );
+  }
+</script>
+
+<div class="container">   
+     <nav>
+         <div class="nav-wrapper" style="background-color: #2bbbad !important;padding-left: 20px">
+      <a href="#" class="brand-logo">Generazione documenti</a>
+    </div>
+  </nav>
+    <div id="generazionedocumenti"  class="row">
+        <div class="col s8">
+          <div class="row">
+            <div class="input-field col s12">
+              <i class="material-icons prefix">account_circle</i>
+              <input type="text" id="autocomplete-input" name="candidato" class="autocomplete">
+              <label for="autocomplete-input">Candidato</label>
+            </div>
+          </div>
+        <div class="row">
+            <div class="col s4">
+                <input type="text" placeholder="Data documenti" class="datepicker" name="data">
+            </div>
+        </div>
+          <div class="row">
+                <div class="card white darken-1">
+                    <div class="card-content black-text">
+                        <span class="card-title">Documenti</span>
+                        <?php
+                        foreach ($documenti as $key => $documento) {
+                            $checked='';
+                            $docid=$documento['ID'];
+                            if( $docid=='1398' ||
+                                    $docid=='1363' ||
+                                    $docid=='1364' ||
+                                    $docid=='1365' ||
+                                    $docid=='1366' ||
+                                    $docid=='1367' ||
+                                    $docid=='1368' ||
+                                    $docid=='1369' ||
+                                    $docid=='1370' ||
+                                    $docid=='1371' ||
+                                    $docid=='1372' ||
+                                    $docid=='1373' ||
+                                    $docid=='1381' ||
+                                    $docid=='1382' ||
+                                    $docid=='1383' ||
+                                    $docid=='1399'
+                                    )
+                            {
+                            $checked='checked';
+                            }
+                        ?>
+                        <p>
+                            <label>
+                              <input type="checkbox" name="documenti[]" value="<?=$documento['Documento']?>" <?=$checked?>/>
+                              <span><?=$documento['Documento']?></span>
+                            </label>
+                        </p>
+                        <?php   
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>  
+        </div>
+        <div class="col s4" style="padding: 50px;">
+            
+            <div class="row">
+                <div class="btn waves-effect waves-light" onclick="creacopertina()" >Crea Copertina 
+                    <i class="material-icons right">send</i>
+                </div>
+            </div>
+            <div class="row">
+                <div class="btn waves-effect waves-light" onclick="crea()">Crea Documenti
+                    <i class="material-icons right">send</i>
+                </div>
+            </div>
+            <div class="row">
+                <div class="btn waves-effect waves-light" onclick="converti()">Converti
+                    <i class="material-icons right">send</i>
+                </div>
+            </div>
+            
+           
+            <div id="modal1" class="modal">
+                <div class="modal-content">
+                  Processo in corso. Verificare la cartella con i file
+                </div>
+                <div class="modal-footer">
+                  <a href="#!" class="modal-close waves-effect waves-green btn-flat">Chiudi</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
---
+++ application/views/sys/desktop/stampe/easywork_documento.php
@@ -0,0 +1,74 @@
+<?php
+require_once 'phpword/PHPWord.php';
+
+$PHPWord = new PHPWord();
+
+$document = $PHPWord->loadTemplate("../JDocServer/Modelli/$modello");
+
+
+$document->setValue('nomecognome', $nomecognome);
+$document->setValue('cognomenome', $cognomenome);
+$document->setValue('nome', $nome);
+$document->setValue('cognome', $cognome);
+$document->setValue('nomecognomemaiusc', $nomecognomemaiusc);
+$document->setValue('email', $email);
+$document->setValue('dataassunzione', $dataassunzione);
+$document->setValue('indirizzo', $indirizzo);
+$document->setValue('cap', $cap);
+$document->setValue('paese', $paese);
+$document->setValue('numeroavs', $numeroavs);
+$document->setValue('dd', $datanascita_dd);
+$document->setValue('mm', $datanascita_mm);
+$document->setValue('yyyy', $datanascita_yyyy);
+$document->setValue('sm', $sesso_m);
+$document->setValue('sf', $sesso_f);
+$document->setValue('ni', $nazione_i);
+$document->setValue('telefono', $telefono);
+$document->setValue('sc1', $sc1);
+$document->setValue('sc3', $sc3);
+$document->setValue('sc6', $sc6);
+$document->setValue('scdd', $datastatocivile_dd);
+$document->setValue('scmm', $datastatocivile_mm);
+$document->setValue('scyyyy', $datastatocivile_yyyy);
+
+$document->setValue('spcognome', $spcognome);
+$document->setValue('spnome', $spnome);
+$document->setValue('spnumeroavs', $spnumeroavs);
+$document->setValue('spdatanascitadd', $spdatanascitadd);
+$document->setValue('spdatanascitamm', $spdatanascitamm);
+$document->setValue('spdatanascitayyyy', $spdatanascitayyyy);
+$document->setValue('spindirizzo', $spindirizzo);
+$document->setValue('spcap', $spcap);
+$document->setValue('sppaese', $sppaese);
+$document->setValue('spdataattivitadd', $spdataattivitadd);
+$document->setValue('spdataattivitamm', $spdataattivitamm);
+$document->setValue('spdataattivitayyyy', $spdataattivitayyyy);
+$document->setValue('spredditoestero', $spredditoestero);
+$document->setValue('spredditosvizzera', $spredditosvizzera);
+
+$document->setValue('funocognomenome', "$f1cognome $f1nome");
+$document->setValue('funodatadinascitadd', $f1datadinascitadd);
+$document->setValue('funodatadinascitamm', $f1datadinascitamm);
+$document->setValue('funodatadinascitayyyy', $f1datadinascitayyyy);
+
+
+$document->setValue('fduecognomenome', "$f2cognome $f2nome");
+$document->setValue('fduedatadinascitadd', $f2datadinascitadd);
+$document->setValue('fduedatadinascitamm', $f2datadinascitamm);
+$document->setValue('fduedatadinascitayyyy', $f2datadinascitayyyy);
+
+$document->setValue('ftrecognomenome', "$f3cognome $f3nome");
+$document->setValue('ftredatadinascitadd', $f3datadinascitadd);
+$document->setValue('ftredatadinascitamm', $f3datadinascitamm);
+$document->setValue('ftredatadinascitayyyy', $f3datadinascitayyyy);
+
+$document->setValue('altreattivitasi', $altreattivitasi);
+$document->setValue('altreattivitano', $altreattivitano);
+
+
+
+
+
+
+$document->save('../JDocServer/DocumentiGenerati/'.$nomecognome.'-'.$id.'-'.$documento);
+?>
